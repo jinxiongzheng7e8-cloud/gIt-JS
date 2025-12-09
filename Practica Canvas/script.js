0a5c0085f4e49a9b08c4165a3c34ededa03f93be
JinxiongZheng <jinxiong.zheng.7e8@itb.cat>
2025-12-09 19:10:17 +0100

    Add Ejercicios: Examen and write the problem code.

---
+++ Practica Canvas/script.js
@@ -0,0 +1,10 @@
+let titulo = document.getElementById("titulo");
+console.log("Elemento con Id=titulo: " + titulo);
+
+
+
+let elemento = document.getElementById("elemento");
+console.log("nodeName: " + elemento.nodeName);
+console.log("textContent: " + elemento.textContent);
+console.log("innerHTML: " + elemento.innerHTML);
+console.log("outerHTML: " + elemento.outerHTML);
